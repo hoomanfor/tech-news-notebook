d8e0bbded082a2c7263f0b1ddb57b74b96d02b89
Hoo Man <hoomanfor@gmail.com>
2020-02-01 09:40:41 -0500

    added mobile friendly nav

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,1 +1,8 @@
-console.log("logic.js works!")
+
+function classToggle() {
+    const navs = document.querySelectorAll('.nav-items')
+    navs.forEach(nav => nav.classList.toggle('toggle-show'));
+}
+
+document.querySelector('.toggle').addEventListener('click', classToggle);
+
